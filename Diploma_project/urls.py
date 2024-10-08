320842daa7f827e36e7757abeaa110e361bbe317
andriimilevskyi <andrey.mil204@gmail.com>
2024-10-08 02:15:01 +0200

    Adding drf_yasg urls (swagger etc.)

--- Diploma_project/urls.py
+++ Diploma_project/urls.py
@@ -17,10 +17,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="iPhone Cases API",
+        default_version='v1',
+        description="API documentation for iPhone Cases",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@iphonecases.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
     path('api/', include('webapp.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('', TemplateView.as_view(template_name="vault_web/build/index.html")),
 ]
