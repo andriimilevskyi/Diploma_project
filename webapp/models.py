d7b5b4a594db43fa4548ba58c7af346ff22bbc46
andriimilevskyi <andrey.mil204@gmail.com>
2024-10-08 02:01:52 +0200

    creating model template

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Case(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    material = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
