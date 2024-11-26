16e9560c9b431a366f6f211830da21fa41feee19
andriimilevskyi <andrey.mil204@gmail.com>
2024-11-26 19:36:57 +0100

    Adding and modernization of urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import CaseListCreate, CaseRetrieveUpdateDestroy
+from .views import CaseViewSet, OrderViewSet, OrderItemViewSet
 
 urlpatterns = [
-    path('cases/', CaseListCreate.as_view(), name='case-list-create'),  # List and create cases
-    path('cases/<int:pk>/', CaseRetrieveUpdateDestroy.as_view(), name='case-detail'),
-    # Retrieve, update, or delete case
+    # Cases
+    path('cases/', CaseViewSet.as_view({'get': 'list', 'post': 'create'}), name='case-list'),
+    path('cases/<int:pk>/', CaseViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='case-detail'),
+
+    # Orders
+    path('orders/', OrderViewSet.as_view({'get': 'list', 'post': 'create'}), name='order-list'),
+    path('orders/<int:pk>/', OrderViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='order-detail'),
+
+    # Order Items
+    path('order-items/', OrderItemViewSet.as_view({'get': 'list', 'post': 'create'}), name='orderitem-list'),
+    path('order-items/<int:pk>/', OrderItemViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='orderitem-detail'),
 ]
