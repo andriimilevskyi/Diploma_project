2160d9d7ac7e1383626218ef8c61cae95ed64e25
andriimilevskyi <andrey.mil204@gmail.com>
2025-04-26 20:32:43 +0200

    Adding api get by ID for mtb and road: - changing urls to routing - views modernization and mixins using - adding retrieve method for single mtb and road

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,21 +1,32 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 # from .views import CaseViewSet, OrderViewSet, OrderItemViewSet
+from rest_framework.routers import DefaultRouter
 from .views import MTBBikeViewSet, RoadBikeViewSet
 
-urlpatterns = [
-    # Cases
-    path('mtb/', MTBBikeViewSet.as_view({'get': 'list', 'post': 'create'}), name='mtb-list'),
-    path('road/', RoadBikeViewSet.as_view({'get': 'list', 'post': 'create'}), name='road-list'),
-    # path('cases/<int:pk>/', CaseViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
-    #      name='case-detail'),
-    #
-    # # Orders
-    # path('orders/', OrderViewSet.as_view({'get': 'list', 'post': 'create'}), name='order-list'),
-    # path('orders/<int:pk>/', OrderViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
-    #      name='order-detail'),
-    #
-    # # Order Items
-    # path('order-items/', OrderItemViewSet.as_view({'get': 'list', 'post': 'create'}), name='orderitem-list'),
-    # path('order-items/<int:pk>/', OrderItemViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
-    #      name='orderitem-detail'),
-]
+router = DefaultRouter()
+router.register(r'mtb', MTBBikeViewSet, basename='mtb')
+router.register(r'road', RoadBikeViewSet, basename='road')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+
+# path('mtb/', MTBBikeViewSet.as_view({'get': 'list', 'post': 'create'}), name='mtb-list'),
+#
+# path('road/', RoadBikeViewSet.as_view({'get': 'list', 'post': 'create'}), name='road-list'),
+
+# Cases
+# path('cases/<int:pk>/', CaseViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
+#      name='case-detail'),
+#
+# # Orders
+# path('orders/', OrderViewSet.as_view({'get': 'list', 'post': 'create'}), name='order-list'),
+# path('orders/<int:pk>/', OrderViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
+#      name='order-detail'),
+#
+# # Order Items
+# path('order-items/', OrderItemViewSet.as_view({'get': 'list', 'post': 'create'}), name='orderitem-list'),
+# path('order-items/<int:pk>/', OrderItemViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
+#      name='orderitem-detail'),
+# ]
